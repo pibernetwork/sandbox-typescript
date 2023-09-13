4c4ef36faeba208ece96a162e1397658e677e726
Mauricio Piber <mauriciopiber@gmail.com>
2023-09-13 13:02:27 -0300

    Added version to uppercase class

--- decorators/typescript5experimental/src/classes/uppercaste.test.ts
+++ decorators/typescript5experimental/src/classes/uppercaste.test.ts
@@ -6,5 +6,7 @@ describe('Class Decorators - The Uppercase case', () => {
     const newExample = new ClassUppercaseCase('My name', 'my city');
 
     expect(newExample.displayInfo()).toEqual('Name: MY NAME, City: MY CITY');
+    /** @ts-expect-error Version will not work in typescript because types can't change */
+    expect(newExample.version).toEqual('1.0.0');
   });
 });
